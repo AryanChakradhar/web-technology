01fac0cd5932a41c988e465b34e3d66dd73484d8
aryanchakradhar <aryanchakradhar11@gmail.com>
2023-04-28 23:34:36 +0545

    Add layout files

---
+++ area.js
@@ -0,0 +1,4 @@
+let a=5;
+let b=5;
+let area=a*b;
+console.log ("area of rectangle",area);
---
+++ hello.js
@@ -0,0 +1,10 @@
+let a=5;
+console.log (typeof(a));
+a=6
+console.log(a);
+var b=7;
+b=10;
+console.log(a);
+
+const n=50;
+console.log(n);
